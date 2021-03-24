997d9dcf89d8fd872f7645cf9c53a0944a8d61ac
Maksym Haponov <gaponovm08@gmail.com>
2021-03-23 22:41:24 -0400

    implement drawer and dropdown menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,17 +7,35 @@ import Toolbar from '@material-ui/core/Toolbar'
 import EmailIcon from '@material-ui/icons/Email'
 import LinkedinIcon from '@material-ui/icons/LinkedIn'
 import FacebookIcon from '@material-ui/icons/Facebook'
+import GithubIcon from '@material-ui/icons/GitHub'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import HideOnScroll from './HideOnScroll'
 import BackToTop from "./BackToTop"
-
+import { makeStyles } from '@material-ui/core/styles';
 import {
     Fab,
     Menu,
+    MenuList,
     MenuItem,
     Container,
     Hidden
 } from "@material-ui/core";
 import { KeyboardArrowUp } from "@material-ui/icons";
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import SideDrawer from '../components/SideDrawer'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    paper: {
+        marginRight: theme.spacing(2),
+    },
+}));
 
 const styles = {
     button: {
@@ -31,39 +49,88 @@ const styles = {
 }
 
 export default function NavBar() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+        setOpen(false);
+    };
+
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen(false);
+        }
+    }
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+    React.useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus();
+        }
+        prevOpen.current = open;
+    }, [open]);
+
     return (
         <header style={{ backgroundColor: "#1F2833", minHeight: "64px" }} >
+            <Toolbar id="back-to-top-anchor" />
             <HideOnScroll style={{ minHeight: "64px" }}>
                 <AppBar position="fixed" style={{ minHeight: "64px" }}>
                     <Toolbar color="primary" style={{ minHeight: "64px" }}>
-                        <Container xs="12">
+                        <Container>
                             <Grid container
-                                justify="space-between">
+                                justify="space-between"
+                                alignItems="center">
                                 <Grid item>
                                     <Hidden mdUp>
-                                        Sandwich Drawer Menu
+                                        <SideDrawer/>
                                     </Hidden>
                                     <Hidden smDown>
                                         <Button style={styles.button} href="#home">Home</Button>
                                         <Button style={styles.button} href="#about" >About me</Button>
                                         <Button style={styles.button} href="#my-projects">My Projects</Button>
-                                        <Button style={styles.button}>Social media</Button>
-                                        <Menu>
-                                            <MenuItem leftIcon={<LinkedinIcon />}>
-                                                LinkedIn
-                                        </MenuItem>
-                                            <MenuItem startIcon={<FacebookIcon />}>
-                                                Facebook
-                                        </MenuItem>
-                                        </Menu>
+                                        <Button
+                                            ref={anchorRef}
+                                            aria-controls={open ? 'menu-list-grow' : undefined}
+                                            aria-haspopup="true"
+                                            onClick={handleToggle}
+                                            endIcon={ open ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
+                                            style={styles.button}>
+                                            Social media
+                                            </Button>
+                                        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                                            {({ TransitionProps, placement }) => (
+                                                <Grow
+                                                    {...TransitionProps}
+                                                    style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}>
+                                                    <Paper>
+                                                        <ClickAwayListener onClickAway={handleClose}>
+                                                            <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                                                <MenuItem onClick={handleClose}><LinkedinIcon/>  LinkedIn</MenuItem>
+                                                                <MenuItem onClick={handleClose}><FacebookIcon/>  Facebook</MenuItem>
+                                                                <MenuItem onClick={handleClose}><GithubIcon/>  GitHub</MenuItem>
+                                                            </MenuList>
+                                                        </ClickAwayListener>
+                                                    </Paper>
+                                                </Grow>
+                                            )}
+                                        </Popper>
                                     </Hidden>
                                 </Grid>
                                 <Grid item justifyItems="center">
-                                    <Button 
+                                    <Button
                                         href="mailto:gaponovm08@gmail.com"
                                         variant="contained"
                                         color="secondary"
-                                        
                                         startIcon={<EmailIcon />}>
                                         Contact me
                                     </Button>
@@ -73,7 +140,6 @@ export default function NavBar() {
                     </Toolbar>
                 </AppBar>
             </HideOnScroll>
-            <Toolbar id="back-to-top-anchor" />
             <BackToTop>
                 <Fab color="secondary" size="large" aria-label="scroll back to top">
                     <KeyboardArrowUp />
--- src/components/SideDrawer.js
+++ src/components/SideDrawer.js
@@ -1,27 +1,141 @@
-import * as React from "react";
-import { Drawer } from "@material-ui/core";
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import Button from '@material-ui/core/Button';
 
+const drawerWidth = 250;
 
-class LeftDrawer extends React.Component {
-    state = {
-      open: false,
-    };
-  
-    handleDrawerOpen = () => {
-      this.setState({ open: true });
-    };
-  
-    handleDrawerClose = () => {
-      this.setState({ open: false });
-    };
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    backgroundColor: '#1F2833',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
+export default function PersistentDrawerLeft() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root} >
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={handleDrawerOpen}
+        edge="start"
+        className={clsx(classes.menuButton, open && classes.hide)}>
+        <MenuIcon />
+      </IconButton>
+      <Drawer
+        className={classes.drawer}
+        variant="Temporary"
+        anchor="left"
+        open={open}
+        onClose={handleDrawerClose}
+        classes={{
+          paper: classes.drawerPaper,
+        }}>
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        {/* DRY */}
+        <List>
+          <ListItem onClick={handleDrawerClose} onKeyDown={handleDrawerClose} component={Button} href="#home">
+            <ListItemIcon><MailIcon /></ListItemIcon>
+            <ListItemText primary='Home' />
+          </ListItem>
+          <ListItem onClick={handleDrawerClose} onKeyDown={handleDrawerClose} component={Button} href="#about">
+            <ListItemIcon><MailIcon /></ListItemIcon>
+            <ListItemText primary='About me' />
+          </ListItem>
+          <ListItem onClick={handleDrawerClose} onKeyDown={handleDrawerClose} component={Button} href="#my-projects">
+            <ListItemIcon><MailIcon /></ListItemIcon>
+            <ListItemText primary='My projects' />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon><MailIcon /></ListItemIcon>
+            <ListItemText primary='Social Media' />
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
+  );
 }
-
-const SideDrawer  = ({ children }) => {
-    return (
-        <Drawer>
-            {children}
-        </Drawer>
-    );
-};
-
-export default SideDrawer;
